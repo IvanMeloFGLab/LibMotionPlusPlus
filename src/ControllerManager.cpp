5d046a966e14c9b6b694b77f1216b328a13a26df
Ivan Melo <ivanmelo@ijaxxa.com>
2026-08-10 10:34:52 -0600

    Fixed polling bug.

--- src/ControllerManager.cpp
+++ src/ControllerManager.cpp
@@ -19,7 +19,7 @@ using milis = std::chrono::milliseconds;
 
 using namespace motionplusplus;
 
-ControllerManager::ControllerManager() : device_num_(0), scan_interval_(milis(250)), last_scan_(steady_clock::now()), new_controllers_(false) {
+ControllerManager::ControllerManager() : device_num_(0), scan_interval_(milis(500)), last_scan_(steady_clock::now()), new_controllers_(false) {
 
 }
 
@@ -84,16 +84,16 @@ expected<void, error_code> ControllerManager::connect() {
 expected<void, error_code> ControllerManager::update(milis timeout) {
   if (steady_clock::now() - last_scan_ >= scan_interval_) {
     last_scan_ = steady_clock::now();
-    auto input_devices = dm_.scan();
-    if (!input_devices) return unexpected(input_devices.error());
-    if (static_cast<int>(input_devices->size()) > device_num_) {
+    auto raw_devices = dm_.scan();
+    if (!raw_devices) return unexpected(raw_devices.error());
+    if (static_cast<int>(raw_devices->size()) != device_num_) {
       auto scn = scan();
       if (!scn) return unexpected(scn.error());
       if (new_controllers_) {
         auto cn = connect();
         if (!cn) return unexpected(cn.error());
       }
-      device_num_ = input_devices->size();
+      device_num_ = raw_devices->size();
     }
   }
 
